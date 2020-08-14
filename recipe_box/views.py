09fef5ffb8d0e515de1fd72229dc6717e03665d4
kbrooks87 <kbrooks10011987@gmail.com>
2020-08-14 16:40:55 -0400

    made logout take you back to homepage instead of login page

--- recipe_box/views.py
+++ recipe_box/views.py
@@ -75,7 +75,7 @@ def login_view(request):
 
 def logout_view(request):
     logout(request)
-    return HttpResponseRedirect(reverse("loginview"))
+    return HttpResponseRedirect(reverse("homepage"))
 
 
 def signup_view(request):
